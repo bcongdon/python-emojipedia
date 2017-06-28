ca2b5f06123ae3485053e40c0d8ef58c1ce4e95b
Benjamin Congdon <bencongdon96@gmail.com>
2017-06-27 18:56:12 -0700

    Version 0.3.3

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ from distutils.core import setup
 
 setup(name='Emojipedia',
       packages=['emojipedia'],
-      version='0.3.2',
+      version='0.3.3',
       description='Emoji data from Emojipedia',
       author='Benjamin Congdon',
       author_email='bcongdo2@illinois.edu',
       license='MIT',
       url='https://github.com/benjamincongdon',
-      download_url='https://github.com/benjamincongdon/' +
-                   'python-emojipedia/tarball/0.2',
       keywords=['emoji', 'emojipedia'],
       classifiers=['Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.6',
